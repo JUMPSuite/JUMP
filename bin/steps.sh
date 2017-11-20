7e6c24559bd6c963e498783dbb4e1e7b952abed9
Stephen Espy <stephen.espy@stjude.org>
2017-11-19 22:47:39 -0600

    iterate

--- bin/steps.sh
+++ bin/steps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-this_work_dir=$1
+#this_work_dir=$1
 this_scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 dtn=`date +%Y%m%d.%N`
 pwd="$(pwd)"
-#this_work_dir="/home/sespy/work/proteomics/jump-search"
+this_work_dir="/home/sespy/work/proteomics/jump-search"
 
 echo "start (steps) $this_scripts_dir"
 echo "pwd $pwd"
@@ -12,8 +12,8 @@ echo "this_scripts_dir $this_scripts_dir"
 echo "this_work_dir $this_work_dir"
 echo "dtn $dtn"
 
-$this_scripts_dir/step_1.sh $pwd $dtn
-$this_scripts_dir/step_2.sh $pwd $dtn
+$this_scripts_dir/step_1.sh $this_work_dir $dtn
+$this_scripts_dir/step_2.sh $this_work_dir $dtn
 
 echo "done. (steps)"
 
