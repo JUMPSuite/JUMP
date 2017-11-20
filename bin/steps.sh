994422d41c5d6fd05cb1b49c1376f4dff851ac91
Stephen Espy <stephen.espy@stjude.org>
2017-11-19 22:21:22 -0600

    iterate

--- bin/steps.sh
+++ bin/steps.sh
@@ -5,11 +5,12 @@ dtn=`date +%Y%m%d.%N`
 pwd="$(pwd)"
 this_work_dir=$1
 
-echo "start (steps)" $this_scripts_dir
-echo "pwd" $pwd
+echo "start (steps) $this_scripts_dir"
+echo "pwd $pwd"
+echo "dtn $dtn"
 
-$this_scripts_dir/step_1.sh $this_work_dir $dtn
-$this_scripts_dir/step_2.sh $this_work_dir $dtn
+#$this_scripts_dir/step_1.sh $this_work_dir $dtn
+#$this_scripts_dir/step_2.sh $this_work_dir $dtn
 
 echo "done. (steps)"
 
