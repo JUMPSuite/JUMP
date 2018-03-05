d69e00e95e5b59f47f0c8088ea525a1aab8cc9fe
Alex Breuer <alex.breuer@stjude.org>
2018-03-05 10:39:08 -0600

    queue and memory switches

--- bin/jump_f.pl
+++ bin/jump_f.pl
@@ -5,6 +5,7 @@ our $VERSION = 12.1.0;
 use File::Basename;
 use Cwd 'abs_path';
 use File::Spec;
+use Getopt::Long;
 
 print <<EOF;
 
@@ -21,6 +22,18 @@ print <<EOF;
 #                                                              #
 ################################################################
 EOF
+my $queue;
+my $mem;
+GetOptions('--queue=s'=>\$queue, '--memory=s'=>\$mem);
+
+if(defined($queue) && !defined($mem)) {
+    $mem = 262144;
+}
+elsif(!defined($queue)) { 
+    $queue = 'large_mem';
+    $mem = 2097152;
+}
+
 unless( scalar(@ARGV) > 0 ) { print "\tusage: jump_f.pl <parameter file>\n"; exit(1); }
-$cmd="bsub -P prot -q large_mem -R \"rusage[mem=2097152]\" -Ip _jump_f.pl" . " " . $ARGV[0];
+$cmd="bsub -P prot -q $queue -R \"rusage[mem=$mem]\" -Ip _jump_f.pl" . " " . $ARGV[0];
 system($cmd);
