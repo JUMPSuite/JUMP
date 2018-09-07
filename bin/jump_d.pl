d0c315247a47eb6d80ac3d183a8149a9c003b89c
Alex Breuer <alex.breuer@stjude.org>
2018-09-07 10:03:05 -0500

    dispatch

--- bin/jump_d.pl
+++ bin/jump_d.pl
@@ -1,11 +1,21 @@
 #!/bin/env perl
 
 use strict;
+use Getopt::Long;
+
+my $dispatch;
+GetOptions('--dispatch=s'=>\$dispatch);
 
 if (scalar(@ARGV) != 1) {
 	print "USAGE:\n\tjump -d jump_d.params\n";
 	exit;
 }
 
-my $cmd = "bsub -P prot -q normal -R \"rusage[mem=20000]\" -Ip _jump_d.pl " . join(" ",@ARGV);
+my $cmd;
+unless(defined($dispatch) && $dispatch eq 'localhost') {
+    my $cmd = "bsub -P prot -q normal -R \"rusage[mem=20000]\" -Ip _jump_d.pl " . join(" ",@ARGV);
+}
+else {
+    $cmd="_jump_d.pl " . $ARGV[0];
+}
 system($cmd); 
