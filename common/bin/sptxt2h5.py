5a30237c2f96b73a63c93925f7d9b5f91fe2a8e2
Alex Breuer <alex.breuer@stjude.org>
2020-05-22 14:15:45 -0500

    sptxt2h5 done

--- common/bin/sptxt2h5.py
+++ common/bin/sptxt2h5.py
@@ -71,3 +71,72 @@ class OutOfCoreMerger:
 
         arr_data,kvdata = self.read(t[1],j)
         return (t[0],arr_data,kvdata)
+
+
+if __name__ == '__main__':
+    import optparse
+    p = optparse.OptionParser()
+    p.add_option( '-v', '--verbose', dest='verbose', action='store_true' )
+    p.add_option( '-n', '--num-spectra', dest='num_spectra', type='int', default=0 )
+
+    opts,args = p.parse_args()
+
+    sorter = OutOfCoreMerger(8192)
+    num_written = 0
+    for sptxtfile in args[:-1]:
+        if opts.verbose:
+            if num_written > 0: print('')
+            print( 'processing file {}...'.format(sptxtfile), end='', flush=True )
+        f = open(sptxtfile)
+        for l in f:
+            if re.search( '^Name:', l ):
+                break
+
+        t = l.split(':')
+        mdata = dict([(t[0].strip().lower(),t[1].strip())])
+        mdata['source'] = args[0]
+        mdata['prec_mass'] = mdata['precursormz']
+
+        peaks = []
+        
+        for l in f:
+            if re.search( '^Name:', l ):
+                writer.write_record( np.array(peaks), mdata )
+
+                if opts.num_spectra > 0 and num_written == opts.num_spectra:
+                    break
+
+                t = l.split(':')
+                mdata = dict([(t[0].strip(),t[1].strip())])
+                mdata['Source'] = args[0]
+            
+                mz = []
+                inten = []
+                if num_written % 1000 == 0 and opts.verbose:
+                    print( '{}...'.format(num_written), end='', flush=True )
+                    
+            elif re.search('^\\w+:',l):
+                t = l.split(':')
+                mdata[t[0].strip()] = t[1].strip()
+            elif re.search('^\\d+',l):
+                t = l.split('\t')
+                mz.append( float(t[0]) )
+                inten.append( float(t[1]) )
+            
+        if not opts.num_spectra > 0 or not num_written == opts.num_spectra:
+            sorter.push_record( float(mdata['prec_mz']), {'mz':np.array(mz),
+                                                          'inten':np.array(inten)}, mdata )
+            num_written += 1
+
+if opts.verbose: 
+    print( 'done.' )
+    print( 'Sorting masses...', end='', flush=True )
+
+with sd.CSRSpectralDataWriter(sys.argv[-1]) as w:
+    for i,(mass,arrd,metad) in enumerate(sorter):
+        if i % 1000 == 0 and opts.verbose:
+            print( '{}...'.format(num_written), end='', flush=True )
+        w.write_record( np.hstack((arrd['mz'].reshape((-1,1)),arrd['inten'].reshape((-1,1)))), metad )
+
+if opts.verbose: 
+    print( 'done.' )
