8a6f0ad6200901a236ba567fff3895966e2d317d
Yuan <zyuan1@splprhpc06.cm.cluster>
2021-01-27 18:31:06 -0600

    in JUMP-comet_pipeline.py, correct the usage of print by using parentheses

--- JUMPsj/bin/JUMP-comet_pipeline.py
+++ JUMPsj/bin/JUMP-comet_pipeline.py
@@ -515,9 +515,9 @@ def mzXMLtoMS2(mzXML):
 cmd = "jump -deisotope "+jump_params+" "+" ".join(mzXMLs)
 os.system(cmd)
 
-print "################################################################"
-print "#       ****  Starting COMET search                 ****       #"
-print "################################################################\n\n"
+print ("################################################################\n")
+print ("#       ****  Starting COMET search                 ****       #\n")
+print ("################################################################\n\n")
 
 #cometParams = "comet_HH_tmt10_mouse.params"
 if (mzXMLs == ["*.mzXML"]) or (glob.glob(mzXML_path+"/*.mzXML")==[]):
